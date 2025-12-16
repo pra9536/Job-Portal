8106f6c231666d078f168cfb9af168acd324e701
Prateek Yadav <prateek246729@gmail.com>
2025-12-17 01:05:47 +0530

    change

--- frontend/src/Services/AuthService.tsx
+++ frontend/src/Services/AuthService.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const base_url = `${import.meta.env.VITE_API_BASE_URL}/auth`;
+const base_url = `${process.env.REACT_APP_API_BASE_URL}/auth`;
 
 const loginUser = async (login: any) => {
   const res = await axios.post(`${base_url}/login`, login);
